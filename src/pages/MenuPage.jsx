f96680c8249fb326f6486158d4ca3b657b5aed71
최선화 <161446039+sunhew@users.noreply.github.com>
2024-06-24 15:16:19 +0900

    Update MenuPage.jsx

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import 'react-datepicker/dist/react-datepicker.css';
 import '../assets/scss/style.scss';
 import Main from '../components/section/Main';
 
